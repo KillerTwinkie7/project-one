d00053e011edf9190e0e63c9c6b6d4855b866a74
KillerTwinkie7 <87150610+KillerTwinkie7@users.noreply.github.com>
2024-04-24 18:07:51 -0500

    Add files via upload

--- crime_analysis.py
+++ crime_analysis.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 
-def make_df(path):
+def make_df(path): # Reads the csv file in and makes it a DataFrame
     df = pd.read_csv(path)
     return df
 
@@ -14,4 +14,4 @@ dir_string = str(directory) + "/data/crime_data.csv"
 all_data_df = make_df(dir_string)
 print(all_data_df.head())
 
-
+print(list(all_data_df))
